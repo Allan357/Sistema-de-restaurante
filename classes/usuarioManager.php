bf2330f399c2c48adb24434741d1cb69a82b2081
ygor2812 <ygordeandrade3222@outlook.com>
2025-11-10 15:52:59 -0400

    [RF-6] Implementação do sistema de autenticação - Ygor de Andrade

---
+++ classes/usuarioManager.php
@@ -0,0 +1,61 @@
+<?php
+require_once 'jsonStorage.php';
+
+class UsuarioManager {
+    private $storage;
+
+    public function __construct() {
+        $this->storage = new JsonStorage('../data/usuarios.json');
+        $this->criarAdmin();
+    }
+
+    private function criarAdmin() {
+        $usuarios = $this->storage->read();
+        if (empty($usuarios)) {
+            $admin = [
+                'id' => 1,
+                'nome' => 'Admin',
+                'login' => 'admin',
+                'senha' => password_hash('admin123', PASSWORD_DEFAULT),
+                'tipo' => 'admin'
+            ];
+            $this->storage->write([$admin]);
+        }
+    }
+
+    public function login($login, $senha) {
+        $usuarios = $this->storage->read();
+        foreach ($usuarios as $user) {
+            if ($user['login'] === $login && password_verify($senha, $user['senha'])) {
+                return [
+                    'id' => $user['id'],
+                    'nome' => $user['nome'],
+                    'tipo' => $user['tipo']
+                ];
+            }
+        }
+        throw new Exception('Login ou senha inválidos.');
+    }
+
+    public function cadastroGarcom($nome, $login, $senha) {
+        $usuarios = $this->storage->read();
+
+        foreach ($usuarios as $user) {
+            if ($user['login'] === $login) {
+                throw new Exception('Login já existente.');
+            }
+        }
+
+        $id = count($usuarios) + 1;
+        $usuarios[] = [
+            'id' => $id,
+            'nome' => $nome,
+            'login' => $login,
+            'senha' => password_hash($senha, PASSWORD_DEFAULT),
+            'tipo' => 'garcom'
+        ];
+
+        $this->storage->write($usuarios);
+        return "Garçom '$nome' cadastrado com sucesso.";
+    }
+}
