94f4a1d9cd76ef91012a7b8a6f2588dfaef3fb99
Pablo Barreto do Nascimento <diariamentepablo@gmail.com>
2025-11-17 19:32:09 -0400

    Alteração - Página stand-in para relatório

---
+++ pages/relatorio.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relatório</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+    <?php include '../includes/navbar.php'; ?>
+
+    <div>
+        <h1>Sentimos muito, mas parece que está página está em outro castelo.</h1>
+    </div>
+</body>
+</html>
